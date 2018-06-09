57a57983c5d8d45683546f6d3b1172b31b28f419
zhaocc <450959507@qq.com>
2018-06-09 17:46:40 +0800

    add completeTest

---
+++ src/com/baidu/zhaocc/CompleteTest.java
@@ -0,0 +1,115 @@
+package com.baidu.zhaocc;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class CompleteTest {
+
+	private static SessionFactory factory; 
+	   public static void main(String[] args) {
+	      try{
+	         factory = new Configuration().configure("Employee.cfg.xml").buildSessionFactory();
+	      }catch (Throwable ex) { 
+	         System.err.println("Failed to create sessionFactory object." + ex);
+	         throw new ExceptionInInitializerError(ex); 
+	      }
+	      CompleteTest ME = new CompleteTest();
+
+	      /* Add few employee records in database */
+	      Integer empID1 = ME.addEmployee("Zara", "Ali");
+	      Integer empID2 = ME.addEmployee("Daisy", "Das");
+	      Integer empID3 = ME.addEmployee("John", "Paul");
+
+	      /* List down all the employees */
+	      ME.listEmployees();
+
+	      /* Update employee's records */
+	      ME.updateEmployee(empID1, "hhaha");
+
+	      /* Delete an employee from the database */
+	      ME.deleteEmployee(empID2);
+
+	      /* List down new list of the employees */
+	      ME.listEmployees();
+	   }
+	   /* Method to CREATE an employee in the database */
+	   public Integer addEmployee(String fname, String lname){
+	      Session session = factory.openSession();
+	      Transaction tx = null;
+	      Integer employeeID = null;
+	      try{
+	         tx = session.beginTransaction();
+	         Employee employee = new Employee(fname, lname);
+	         employeeID = (Integer) session.save(employee); 
+	         tx.commit();
+	      }catch (HibernateException e) {
+	         if (tx!=null) tx.rollback();
+	         e.printStackTrace(); 
+	      }finally {
+	         session.close(); 
+	      }
+	      return employeeID;
+	   }
+	   /* Method to  READ all the employees */
+	   public void listEmployees( ){
+	      Session session = factory.openSession();
+	      Transaction tx = null;
+	      try{
+	         tx = session.beginTransaction();
+	         List employees = session.createQuery("FROM Employee").list(); 
+	         for (Iterator iterator = 
+	                           employees.iterator(); iterator.hasNext();){
+	            Employee employee = (Employee) iterator.next(); 
+	            System.out.print("First Name: " + employee.getFirstName()); 
+	            System.out.print("  Last Name: " + employee.getLastName()); 
+	         }
+	         tx.commit();
+	      }catch (HibernateException e) {
+	         if (tx!=null) tx.rollback();
+	         e.printStackTrace(); 
+	      }finally {
+	         session.close(); 
+	      }
+	   }
+	   /* Method to UPDATE salary for an employee */
+	   public void updateEmployee(Integer EmployeeID, String firstName ){
+	      Session session = factory.openSession();
+	      Transaction tx = null;
+	      try{
+	         tx = session.beginTransaction();
+	         Employee employee = 
+	                    (Employee)session.get(Employee.class, EmployeeID); 
+	         employee.setFirstName(firstName);
+	         session.update(employee); 
+	         tx.commit();
+	      }catch (HibernateException e) {
+	         if (tx!=null) tx.rollback();
+	         e.printStackTrace(); 
+	      }finally {
+	         session.close(); 
+	      }
+	   }
+	   /* Method to DELETE an employee from the records */
+	   public void deleteEmployee(Integer EmployeeID){
+	      Session session = factory.openSession();
+	      Transaction tx = null;
+	      try{
+	         tx = session.beginTransaction();
+	         Employee employee = 
+	                   (Employee)session.get(Employee.class, EmployeeID); 
+	         session.delete(employee); 
+	         tx.commit();
+	      }catch (HibernateException e) {
+	         if (tx!=null) tx.rollback();
+	         e.printStackTrace(); 
+	      }finally {
+	         session.close(); 
+	      }
+	   }
+}
